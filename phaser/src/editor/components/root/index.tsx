2b2d744bdf51821bd8acd280a6302f6066547cf7
Law Smith <lawsumisu@gmail.com>
2020-12-05 20:05:57 +0900

    improved style of section header.

--- phaser/src/editor/components/root/index.tsx
+++ phaser/src/editor/components/root/index.tsx
@@ -3,6 +3,7 @@ import { Editor } from 'src/editor/components';
 import aero from 'src/characters/aero/frameData';
 import 'src/editor/components/root/styles.scss';
 import FrameDefinitionMapRenderer from 'src/editor/components/frameDefinitionMapRenderer';
+import cx from 'classnames';
 
 export default class HitboxEditor extends React.PureComponent {
   private keys = Object.keys(aero).sort();
@@ -10,11 +11,14 @@ export default class HitboxEditor extends React.PureComponent {
   public render(): React.ReactNode {
     return (
       <div className="cn--root">
-        <div className="root--section">
-          Animations
+        <div className={cx("root--section", 'mod--animations')}>
+          <div className="section--title">Animations</div>
           <FrameDefinitionMapRenderer keys={this.keys} className="cn--animations"/>
         </div>
-        <Editor />
+        <div className="root--section">
+          <div className="section--title">Frame Editor</div>
+          <Editor />
+        </div>
       </div>
     );
   }
