a8dc775a5d54cac05db75dd62056d670d18f81d7
Law Smith <lawsumisu@gmail.com>
2021-03-13 16:33:48 +0900

    removed duplicate raw-loader.

--- phaser/webpack.config.js
+++ phaser/webpack.config.js
@@ -24,10 +24,6 @@ module.exports = {
         test: /\.(gif|png|jpe?g|svg|xml)$/i,
         use: "file-loader"
       },
-      {
-        test: /\.shader$/i,
-        use: 'raw-loader',
-      },
     ]
   },
   devServer: {
